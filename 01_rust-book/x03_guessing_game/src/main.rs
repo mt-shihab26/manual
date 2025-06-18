8e58962e3cd85f71e4df3c64dd30da9a0b036c19
Shihab Mahamud <shihab4t@gmail.com>
2025-06-18 12:36:33 +0600

    stils implement

--- 01_rust-book/x03_guessing_game/src/main.rs
+++ 01_rust-book/x03_guessing_game/src/main.rs
@@ -1,20 +1,14 @@
-mod random;
-
 fn main() {
-    for _ in 0..100 {
-        let result = random::rand();
-        println!("{}", result % 100);
-    }
+    let secret_number = stils::rand_range(1, 100);
 
-    // let secret_number = random::rand();
-    // println!("Secret number: {}", secret_number);
-    //
-    // println!("Guess the number!\nPlease input the guess");
-    //
-    // let mut guess = String::new();
-    // std::io::stdin()
-    //     .read_line(&mut guess)
-    //     .expect("Failed to read line");
-    //
-    // println!("You guessed: {}", guess);
+    println!("Secret number: {}", secret_number);
+
+    println!("Guess the number!\nPlease input the guess");
+
+    let mut guess = String::new();
+    std::io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
