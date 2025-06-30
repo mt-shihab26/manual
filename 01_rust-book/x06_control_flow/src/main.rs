962208d6c2dc04d023f9b1e9ac85c47069d44bce
Shihab Mahamud <shihab4t@gmail.com>
2025-07-01 00:51:44 +0600

    loop label

--- 01_rust-book/x06_control_flow/src/main.rs
+++ 01_rust-book/x06_control_flow/src/main.rs
@@ -38,11 +38,30 @@ pub fn repetation() {
 }
 
 pub fn loop_label() {
-    //
+    let mut count = 0;
+
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if remaining == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
 }
 
 fn main() {
-    // dicision();
+    dicision();
     // repetation();
-    loop_label();
+    // loop_label();
 }
